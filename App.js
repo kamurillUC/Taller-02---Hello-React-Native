9d0285a8565f47e14e9af3f0ae5053a1595f7031
kamurillUC <84762384+kamurillUC@users.noreply.github.com>
2022-03-31 21:07:15 -0600

    commit

--- App.js
+++ App.js
@@ -4,17 +4,33 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.welcome}>Hello React Native!</Text> 
+      <Text style={styles.welcome}>This is my first React Native app</Text> 
+      <Text style={styles.welcome}>This is my first React Native app</Text> 
+      <Text style={styles.welcome}>This is my first React Native app</Text> 
+
       <StatusBar style="auto" />
     </View>
+    
   );
 }
+
+/*Para multiples objetos en el style={styles.welcome}\n 
+se pueden utilizar arreglos como style={[{styles.welcome}, {backgroundColor: 'pink'}]}
+Nota: mejor utilizar solo clases, directamente en style como un arreglo no hace check
+*/
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'black',
     justifyContent: 'center',
+    alignItems: 'center',
+    
   },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
 });
